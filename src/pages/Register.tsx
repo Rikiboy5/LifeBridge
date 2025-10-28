6735c6fa057ee2d13039f8b4697711ed5ddb72fb
Richard Gaál <risog@Bomba-Mac.local>
2025-10-28 21:54:35 +0100

    update

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -46,7 +46,7 @@ export default function Register() {
 
   return (
     <MainLayout>
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-blue-100 dark:from-gray-900 dark:to-gray-800 flex items-center justify-center p-4">
+    <div className="flex items-center justify-center p-4">
       <form
         onSubmit={handleSubmit}
         className="bg-white dark:bg-gray-900 shadow-xl rounded-2xl p-8 w-full max-w-md text-gray-800 dark:text-gray-100 space-y-4"
